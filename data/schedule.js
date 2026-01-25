cb1570c45700c87a312a132943ad2817843883d9
Serhii455 <serhii.maksymiv.ri.2023@lpnu.ua>
2026-01-25 14:45:21 +0200

    my second commit

--- data/schedule.js
+++ data/schedule.js
@@ -189,19 +189,94 @@ export const SCHEDULE = {
       mon: {
         1: {
           subject: "Англійська мова",
-          teacher: "Басюк Т.М.",
-          room: "+PI-21сп525 IV н.к.",
-          type: "lecture",
+          teacher: "Гаврилишин І.",
+          room: "210 ауд.",
+          type: "lab",
         },
+        2: {
+          subject: "Латинська мова",
+          teacher: "Винар С.",
+          room: "210 ауд.",
+          type: "lab",
+        }
       },
       tue: {
         2: {
-          subject: "Вбудовані системи",
-          teacher: "Думич С.С.",
-          room: "215 XI н.к.",
+          subject: "Світоглядне ядро",
+          teacher: "",
+          room: "",
           type: "lab",
         },
+        3: {
+          subject: "Світоглядне ядро",
+          teacher: "",
+          room: "",
+          type: "lab",
+        }
       },
+
+      wed: {
+          1: {
+            subject: "Англійська мова",
+            teacher: "Сарабін О.",
+            room: "208 ауд.",
+            type: "lab",
+          },
+          2: {
+            subject: "Історія філософії II",
+            teacher: "Дахній А.",
+            room: "210 ауд.",
+            type: "lecture",
+          },
+          3: {
+            subject: "Вступ до богословських наук (богослов'я)",
+            teacher: "Басюк Т.М.",
+            room: "+PI-21сп525 IV н.к.",
+            type: "lecture",
+          },
+        },
+
+        thu: {
+          1: {
+            subject: "Історія християнства ІІ",
+            teacher: "Беген О.",
+            room: "210 ауд.",
+            type: "lecture",
+          },
+          2: {
+            subject: "Латинська мова",
+            teacher: "Лемик І.",
+            room: "103 ауд.",
+            type: "lab",
+          },
+          3: {
+            subject: "Вступ до богословських наук (літургіка)",
+            teacher: "Рудейко о. В.",
+            room: "210 ауд.",
+            type: "lecture",
+          },
+        },
+
+        fri: {
+          1: {
+            subject: "Англійська мова",
+            teacher: "Рудейко Н.",
+            room: "305 ауд.",
+            type: "lab",
+          },
+          2: {
+            subject: "Історія філософії II",
+            teacher: "Дахній А.",
+            room: "210 ауд.",
+            type: "lecture",
+          },
+          3: {
+            subject: "Вступ до богословських наук (методологія)",
+            teacher: "Гаврилик с. І.",
+            room: "210 ауд.",
+            type: "lecture",
+          },
+        },
     },
 
     denominator: {}, // на майбутнє
